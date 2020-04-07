5e04487e67b3cc0e8a70294418a398c51ddae054
Priyank Pathak <priyankpathak50@gmail.com>
2020-04-07 07:15:36 -0400

    updates

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
 Credits: https://github.com/jacksonhvisuals/quicksilver
+
+
+Created using the server : `python3 -m http.server`
+
+
+
+### Todo :
+- [ ] google scholar icon
+- [ ] Save temp file for data loading   
+- [ ] parallelize the code (calc_rpn function) ??  
+- [ ] Remove log from tw and th to check if performance changes??
--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 var work = document.getElementById("portfolioItemContainer");
 var workRequest = new XMLHttpRequest();
 
+var publication = document.getElementById("publicationContainer");
+var publicationRequest = new XMLHttpRequest();
+
+
 var link = document.getElementById("socialLinkContent");
 var linkRequest = new XMLHttpRequest();
+
+publicationRequest.open("GET", "json/publication.json", true);
+publicationRequest.onreadystatechange = function () {
+    if (publicationRequest.readyState === 4) {
+        if (publicationRequest.status === 200 || publicationRequest.status == 0) {
+            var html = "";
+            JSON.parse(publicationRequest.responseText).forEach(function (publication) {
+                console.log(publication.name);
+                console.log(html);
+                // html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.arxiv + " target=\"_blank\">arxiv</a></p><p><a href=" + work.github + " target=\"_blank\">github</a></p></div>       <div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";    
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.arxiv + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";    
+            });
+            publication.innerHTML = html;
+        }
+    }
+};
+publicationRequest.send(null);
+
+
 
 linkRequest.open("GET", "json/links.json", true);
 linkRequest.onreadystatechange = function () {
@@ -31,7 +54,7 @@ workRequest.onreadystatechange = function () {
                     html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";    
                 }
                 else{
-                 html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\"> arxiv </a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";       
+                 html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.arxiv + " target=\"_blank\"> arxiv </a></p></div><i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.github + " target=\"_blank\"> github </a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";       
                 }
                 
             });
