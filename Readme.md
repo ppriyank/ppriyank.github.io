03f3f0bcce47b3fc4b469c67866b58b897f1a741
Priyank Pathak <priyankpathak50@gmail.com>
2020-04-07 14:51:26 -0400

    sections

--- Readme.md
+++ Readme.md
@@ -7,6 +7,5 @@ Created using the server : `python3 -m http.server`
 
 ### Todo :
 - [ ] google scholar icon
-- [ ] Save temp file for data loading   
-- [ ] parallelize the code (calc_rpn function) ??  
-- [ ] Remove log from tw and th to check if performance changes??
+- [ ] Thumbnail pictures
+
--- js/index.js
+++ js/index.js
@@ -8,25 +8,6 @@ var publicationRequest = new XMLHttpRequest();
 var link = document.getElementById("socialLinkContent");
 var linkRequest = new XMLHttpRequest();
 
-publicationRequest.open("GET", "json/publication.json", true);
-publicationRequest.onreadystatechange = function () {
-    if (publicationRequest.readyState === 4) {
-        if (publicationRequest.status === 200 || publicationRequest.status == 0) {
-            var html = "";
-            JSON.parse(publicationRequest.responseText).forEach(function (publication) {
-                console.log(publication.name);
-                console.log(html);
-                // html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.arxiv + " target=\"_blank\">arxiv</a></p><p><a href=" + work.github + " target=\"_blank\">github</a></p></div>       <div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";    
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + publication.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + publication.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + publication.arxiv + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + publication.name + "<i class=\"material-icons right\">close</i></span><p>" + publication.description + "</p></div></div></div>";    
-            });
-            publication.innerHTML = html;
-        }
-    }
-};
-publicationRequest.send(null);
-
-
-
 linkRequest.open("GET", "json/links.json", true);
 linkRequest.onreadystatechange = function () {
     if (linkRequest.readyState === 4) {
@@ -49,10 +30,10 @@ workRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(workRequest.responseText).forEach(function (work) {
                 console.log(work.name);
-                console.log(html);
-                if (work.name != "Publication"){
+                // console.log(html);
+                // if (work.name != "Publication"){
                     html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visit</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";    
-                }
+                // }
                 // else{
                  // html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.arxiv + " target=\"_blank\"> arxiv </a></p></div><i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.github + " target=\"_blank\"> github </a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";       
                 // }
@@ -63,3 +44,20 @@ workRequest.onreadystatechange = function () {
     }
 };
 workRequest.send(null);
+
+
+publicationRequest.open("GET", "json/publication.json", true);
+publicationRequest.onreadystatechange = function () {
+    if (publicationRequest.readyState === 4) {
+        if (publicationRequest.status === 200 || publicationRequest.status == 0) {
+            var html = "";
+            JSON.parse(publicationRequest.responseText).forEach(function (publication) {
+                console.log("-------------");
+                console.log(publication.name);
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + publication.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + publication.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + publication.arxiv + " target=\"_blank\">Arxiv</a></p><p><a href=" + publication.github + " target=\"_blank\">Github</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + publication.name + "<i class=\"material-icons right\">close</i></span><p>" + publication.description + "</p></div></div></div>";    
+            });
+            publication.innerHTML = html;
+        }
+    }
+};
+publicationRequest.send(null);
