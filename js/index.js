5efc1e9d908999834dcc251d2f513cffea741734
Priyank Pathak <priyankpathak50@gmail.com>
2020-04-08 03:56:33 -0400

    menu

--- js/index.js
+++ js/index.js
@@ -11,6 +11,25 @@ var projectsRequest = new XMLHttpRequest();
 var link = document.getElementById("socialLinkContent");
 var linkRequest = new XMLHttpRequest();
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+
 linkRequest.open("GET", "json/links.json", true);
 linkRequest.onreadystatechange = function () {
     if (linkRequest.readyState === 4) {
